47505ffa40388cc64fb62bb0d3e7106541adad56
Helio Alves <heleralvess@gmail.com>
2019-07-03 07:30:31 -0300

    Simplificado comportamento da fonte action

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <a className="about" href="#">
+        <a className="action" href="#">
           Sobre
         </a>
       </header>
