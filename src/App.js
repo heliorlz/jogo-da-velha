39129cd4d1a6cd272fd91388c53426359bef018c
Helio Alves <heleralvess@gmail.com>
2019-07-06 15:59:23 -0300

    desafio intermediario aula 49 - adicionado o menuHamburguer como object na page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import Sobre from "./objects/Sobre/index";
 import LogoCollabCode from "./objects/LogoCollabCode/index";
+import MenuHamburguer from "./objects/MenuHamburguer";
 
 function App() {
   return (
     <>
       <LogoCollabCode />
       <Sobre />
+      <MenuHamburguer />
     </>
   );
 }
---
+++ src/objects/MenuHamburguer/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./style.css";
+
+const MenuHamburguer = () => {
+  return (
+    <div className="menu-hamburguer">
+      <div />
+    </div>
+  );
+};
+
+export default MenuHamburguer;
