da26e7b0071e77be18f32cc6565565323f6d23db
Helio Alves <heleralvess@gmail.com>
2019-07-06 12:24:38 -0300

    desafio basico aula 49 - adicionado o logoCollabCode como object na page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 
 import Sobre from "./objects/Sobre/index";
+import LogoCollabCode from "./objects/LogoCollabCode/index";
 
 function App() {
-  return <Sobre />;
+  return (
+    <>
+      <LogoCollabCode />
+      <Sobre />
+    </>
+  );
 }
 
 export default App;
---
+++ src/objects/LogoCollabCode/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import logo from "./logo.png";
+
+const LogoCollabCode = () => {
+  return <img src={logo} alt="Logo Collab Code" />;
+};
+
+export default LogoCollabCode;
