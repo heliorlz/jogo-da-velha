c60b33283e01e1f5daaaacfda81e03ea247f931b
Helio Alves <heleralvess@gmail.com>
2019-07-10 07:22:45 -0300

    Criado tabuleiro basico do projeto

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 import HeaderWrapper from "./components/HeaderWrapper";
+import BoardHash from "./components/BoardHash";
 
 function App() {
   return (
     <>
       <HeaderWrapper />
+      <BoardHash />
     </>
   );
 }
---
+++ src/components/BoardHash/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./style.css";
+
+const BoardHash = () => {
+  return <section className="board-hash" />;
+};
+
+export default BoardHash;
