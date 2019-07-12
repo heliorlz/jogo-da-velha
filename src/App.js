e6712f4a5ba72a9f27a3b68b7eec4b5afa53a589
Helio Alves <heleralvess@gmail.com>
2019-07-12 07:41:18 -0300

    desafio basico aula 51 - criado object Events - checkbox

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import "./App.css";
 
 import HeaderWrapper from "./components/HeaderWrapper";
 import Hashtag from "./components/Hashtag";
+import Events from "./objects/Events";
 
 const App = () => {
   return (
     <main className="app">
       <HeaderWrapper />
       <Hashtag />
+      <Events />
     </main>
   );
 };
---
+++ src/objects/Events/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Events = () => {
+  return (
+    <div>
+      <input type="checkbox" name="events" id="events" />
+      <label htmlFor="events">Mostrar eventos</label>
+    </div>
+  );
+};
+
+export default Events;
